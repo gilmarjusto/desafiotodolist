f151a8502b4a8e8c02cc6166f0868a4548e37b90
gilmarjusto <gilmar.justo2@gmail.com>
2025-04-02 02:15:50 -0300

    Criação da estrutura do projeto

---
+++ src/test/java/com/desafiotodolist/desafiotodolist/DesafiotodolistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.desafiotodolist.desafiotodolist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafiotodolistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
